c4ece40c2ea5ea29a6ea51f1482672067b8da7f7
Cristian <priceputu.cristian@gmail.com>
2017-10-18 23:27:29 +0300

    New files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyxamples',
+    version='1.0',
+    packages=['org', 'org.cp', 'org.cp.pyxamples', 'org.cp.pyxamples.init'],
+    url='',
+    license='',
+    author='price',
+    author_email='',
+    description=''
+)
